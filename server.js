fd001f3bf7fcd302f8c1e1fcb3bbecfdd4c02e55
Kartik8309 <kartiksharma.cse19@chitkarauniversity.edu.in>
2022-04-01 19:26:19 +0530

    add server.js

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ mongoose.connect(DB,{
 
 const app = require("./app");
 
-const port = process.env.PORT || 3000;
+const port = 3000;
 app.listen(port,() => {
     console.log("server started");
 })
