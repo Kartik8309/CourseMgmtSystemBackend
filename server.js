6f3ff7bfbd1b4d3156f6b84f0bf3ae16cebf4702
Kartik8309 <kartiksharma.cse19@chitkarauniversity.edu.in>
2022-04-01 19:23:11 +0530

    add nodemon

--- server.js
+++ server.js
@@ -1,1 +1,21 @@
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config({
+    path:"./config.env"
+})
+
+const DB = process.env.DATABASE;
+
+mongoose.connect(DB,{
+    useNewUrlParser:true
+}).then(conn => {
+    console.log("Connection successfull");
+})
+
+const app = require("./app");
+
+const port = process.env.PORT || 3000;
+app.listen(port,() => {
+    console.log("server started");
+})
