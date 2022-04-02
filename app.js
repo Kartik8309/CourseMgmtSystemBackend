d705f49b5ff99f4c1ebc1504123422e33af41210
Kartik8309 <kartiksharma.cse19@chitkarauniversity.edu.in>
2022-04-02 08:47:51 +0530

    add adminRoutes

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 const express = require("express");
-
+const morgan = require("morgan");
+const adminRouter = require("./routes/adminRoutes"); 
 const app = express();
+
+app.use(express.json())
+app.use(morgan("dev")) //CAHNGE FOR PROD
+
+app.use("/admins",adminRouter);
 
 module.exports = app;
--- routes/adminRoutes.js
+++ routes/adminRoutes.js
@@ -1,1 +1,40 @@
 const express = require("express");
+const router = express.Router();
+const Admin = require("../models/Admin");
+
+router.post("/createAdmin", async (req,res) => {
+    //add middle to check for roleType
+    try {
+        const newAdmin = await Admin.create(req.body);
+        res.status(201).json({
+            status:"success",
+            data:{
+                admin:newAdmin
+            }
+        })
+    } catch (error) {
+        res.status(400).json({
+            status:"fail",
+            message:error
+        })
+    }
+})
+
+router.get("/allAdmins",async(req,res) => {
+    try {
+        const allAdmins = await Admin.find();
+        res.status(200).json({
+            status:"success",
+            data:{
+                admins:allAdmins
+            }
+        })
+    } catch (error) {
+        res.status(404).json({
+            status:"fail",
+            message:error
+        })
+    }
+})
+
+module.exports = router;
