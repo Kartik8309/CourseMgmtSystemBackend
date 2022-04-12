c53304216fecf307ccd7fce59aea10e35703d462
Kartik8309 <kartiksharma.cse19@chitkarauniversity.edu.in>
2022-04-12 23:32:48 +0530

    jwt added and signup route implemented

--- controllers/Auth/studentAuth.js
+++ controllers/Auth/studentAuth.js
@@ -1,13 +1,17 @@
 const Student = require("../../models/Student");
-
+const jwt = require("jsonwebtoken");
+require("dotenv")
 /* student signup contr */
 exports.signup = async(req,res,next) => {
     try {
         const newStudent = await Student.create(req.body);
         /* const {studentAge,studentAddress,studentGender} = req.body; role restrictions ?? */
-        
+        const token = jwt.sign({id:newStudent._id},process.env.JWT_SECRET,{
+            expiresIn:process.env.JWT_EXPIRES_IN
+        })
         return res.status(201).json({
             status:"success",
+            token:token,
             data:{
                 student:newStudent
             }
